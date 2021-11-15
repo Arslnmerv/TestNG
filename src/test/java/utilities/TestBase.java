279524b8779d934029dcfde35f76b0b2c7c1b79d
Arslnmrv <arslanmerve1619@gmail.com>
2021-11-15 10:30:59 +0300

    updated

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -3,8 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
